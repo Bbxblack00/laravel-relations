a3a2833416defcdb5d4652ed3dc749d084faa5c0
Benito <bbxblack@gmail.com>
2021-06-03 18:00:56 +0200

    v0.1

--- LaravelRelations/resources/views/pages/home.blade.php
+++ LaravelRelations/resources/views/pages/home.blade.php
@@ -2,14 +2,12 @@
 
 @section('principal')
 
-{{-- come detto a lezione, cercare di portarsi in pari
-    con l'esercizio visto questa mattina,
-    completando tutto il necessario per operare una CRUD
-    completa sull'entita' definita nella migration
-    che vi allego, passando ovviamente anche per i
-    seeder/factory; cercare in particolar modo di chiarsi
-    gli elementi cardine di tutti i passaggi principali,
-    eventualmente chiedendo se qualcosa non vi e' chiaro --}}
+{{-- come detto a lezione, cercare di mettersi
+ in pari con la live vista in classe,
+ producendo tutto il necessario (dal db alle 2
+ operazioni di crud viste) per le seguenti entita'
+ e relazioni:
+ Pilot <-N----M-> Car <-N----1-> Brand --}}
 
     <h1>HELLO</h1>
     
